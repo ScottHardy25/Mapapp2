e13ddf9591e1ad11bc2f5a7ce24e38ddff80121c
3hards79 <scotthardy25@hotmail.com>
2018-02-22 11:58:13 +0000

    addgs

--- app/src/main/java/com/example/a3hards79/mapapp2/MainActivity.java
+++ app/src/main/java/com/example/a3hards79/mapapp2/MainActivity.java
@@ -14,7 +14,18 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 
-//osmdroid lets us use other maps apart from google street map
+/*osmdroid lets us use other maps apart from google street map
+
+
+this needfs to be added to the build.gradle(module.app)
+with the code:
+dependencies {
+======other dependencies
+compile 'org.osmdroid:osmdroid-android:5.6.5@aar'
+
+}
+
+ */
 import org.osmdroid.config.Configuration;
 //access a database that holds each map tile
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
@@ -30,23 +41,46 @@ public class MainActivity extends AppCompatActivity {
 
     //stores an object with the type of mapview called mp;
 
-    Mapview mp;
+    MapView mp;
 
 //@override forces you to deal with the oncreate. Its like a comment but with an error alet
     @Override
 
-    //onCreate loads when the app is first created 
+    //onCreate loads when the app is first created
     public void onCreate(Bundle savedInstanceState)
     {
+        //super lets us use the methods from the parent oncreate. In this case savedInstanceState
+        //savedInstanceState lets the app save its state while the app is in the background or is
+        // going through a orientation change
         super.onCreate(savedInstanceState);
         // This line sets the user agent, a requirement to download OSM maps
+
+        /*Configuration.getInstance():
+         gets the current reference to the config provider.
+         if one hasn't been set yet,
+         the default provider and default configuration will be used
+         */
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+
 
         setContentView(R.layout.activity_main);
 
-        mv = (MapView)findViewById(R.id.map1);
-        mv.setBuiltInZoomControls(true);
-        mv.getController().setZoom(16);
-        mv.getController().setCenter(new GeoPoint(51.05,-0.72));
+        mp = (MapView)findViewById(R.id.map1);
+        mp.setBuiltInZoomControls(true);
+        //set the zoom level to 16 with 1 being the whole world. 16 is around 2.5 meters per pixal
+        mp.getController().setZoom(16);
+        //sets where on the world we are looking. This is unaffected by zoom
+        mp.getController().setCenter(new GeoPoint(51.05,-0.72));
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu choosemenu){
+
+        //whats the point of this?
+        MenuInflater inflater=getMenuInflater();
+        //R.menu.menu: The first menu is talking about the menu folder, the second menu
+        // is talking about the menu.xml
+        inflater.inflate(R.menu.menu, choosemenu);
+        return true;
     }
 }
